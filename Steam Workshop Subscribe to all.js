6fa08b51ce92beed453560c3ef613d22387ce2c5
Joe X! <jamedinao@gmail.com>
2026-05-28 23:00:59 -0500

    Update script version and button functionality

--- Steam Workshop Subscribe to all.js
+++ Steam Workshop Subscribe to all.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         One-Click Subscribe to all steam workshop items
 // @namespace    https://github.com/joex92/One-Click-Subscribe-to-all-Steam-workshop-items
-// @version      2.4
+// @version      3.0
 // @description  Subscribe to all items shown.
 // @author       JoeX92
 // @match        https://steamcommunity.com/workshop/browse/*
@@ -13,38 +13,38 @@
     'use strict';
     
     window.onload = ()=>{
-        const btn = document.createElement("span");
-        btn.setAttribute("data-panel","{&quot;focusable&quot;:true,&quot;clickOnActivate&quot;:true}");
-        btn.setAttribute("role","button");
-        btn.className = "general_btn createCollection";
-        btn.textContent = ( document.querySelectorAll(".general_btn.subscribe:not(.toggled)").length ) ? "Subscribe to all" : "Unsubscribe to all";
+        const btn = document.createElement("button");
+        btn.setAttribute("type","button");
+        btn.setAttribute("data-accent-color","dull");
+        btn.className = "Thio3V0imwc- _0DdgBbU2bPk- _2RWLTCLE-0s- krQEbDRNCFg-";
+        btn.textContent = ( document.querySelectorAll(".rQvmBxj2Kvg-:empty").length ) ? "Subscribe to all" : "Unsubscribe to all";
         btn.onclick = async (e)=>{
-            const unsubscribeditems = document.querySelectorAll(".general_btn.subscribe:not(.toggled)");
+            const unsubscribeditems = document.querySelectorAll(".rQvmBxj2Kvg-:empty");
             if ( unsubscribeditems.length ) {
                 btn.textContent = "Subscribing...";
                 btn.style.pointerEvents = 'none';
                 unsubscribeditems.forEach( async (i,n)=>{
-                    await i.onclick();
+                    await i.parentElement.querySelector("button").click();
                     console.log(`${n.toString().padStart(2,0)}) Subscribed to item id: ${i.id.match(/(\d+)/)[0]}`);
                 });
                 btn.textContent = "Unsubscribe to all";
                 btn.style.pointerEvents = '';
             } else {
-                const subscribeditems = document.querySelectorAll(".general_btn.subscribe.toggled");
+                const subscribeditems = document.querySelectorAll(".rQvmBxj2Kvg-:not(:empty)");
                 btn.textContent = "Unsubscribing...";
                 btn.style.pointerEvents = 'none';
                 subscribeditems.forEach( async (i,n)=>{
-                    await i.onclick();
+                    await i.parentElement.querySelector("button").click();
                     console.log(`${n.toString().padStart(2,0)}) Unsubscribed to item id: ${i.id.match(/(\d+)/)[0]}`);
                 });
                 btn.textContent = "Subscribe to all";
                 btn.style.pointerEvents = '';
             }
         };
-        const rsection = document.querySelector(".rightSectionHolder>.rightDetailsBlock")
-        if ( rsection ) rsection.insertAdjacentElement("afterbegin",btn);
-        else document.querySelector('.right_column>.panel').insertAdjacentElement("afterbegin",btn);
+        const lsection = document.querySelector("._2u-CXYgdzlo-");
+        if ( document.querySelectorAll(".BfmIgp5hDLQ- > div > div > button").length ) {
+            if ( lsection ) rsection.insertAdjacentElement("afterbegin",btn);
+            else document.querySelector('.VsQBaHlz-3M-').insertAdjacentElement("beforeend",btn);
+        }
     };
-
-    // Your code here...
 })();
