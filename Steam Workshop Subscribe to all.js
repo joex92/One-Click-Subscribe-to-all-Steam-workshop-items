ac0f11e068c634292c140bec2d7303e68150d628
Joe X! <jamedinao@gmail.com>
2026-05-28 23:23:29 -0500

    Update version and refactor subscription logic

--- Steam Workshop Subscribe to all.js
+++ Steam Workshop Subscribe to all.js
@@ -1,52 +1,55 @@
 // ==UserScript==
 // @name         One-Click Subscribe to all steam workshop items
 // @namespace    https://github.com/joex92/One-Click-Subscribe-to-all-Steam-workshop-items
-// @version      3.2
+// @version      4.0
 // @description  Subscribe to all items shown.
 // @author       JoeX92
 // @match        https://steamcommunity.com/workshop/browse/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=steamcommunity.com
-// @grant        none
+// @grant        GM_registerMenuCommand
 // ==/UserScript==
 
 (function() {
     'use strict';
     
-    $(document).ready(()=>{
-        const btn = document.createElement("button");
-        btn.setAttribute("type","button");
-        btn.setAttribute("data-accent-color","dull");
-        btn.className = "Thio3V0imwc- _0DdgBbU2bPk- _2RWLTCLE-0s- krQEbDRNCFg-";
-        btn.textContent = ( document.querySelectorAll(".rQvmBxj2Kvg-:empty").length ) ? "Subscribe to all" : "Unsubscribe to all";
-        btn.onclick = async (e)=>{
+    // $(document).ready(()=>{
+    //     const btn = document.createElement("button");
+    //     btn.setAttribute("type","button");
+    //     btn.setAttribute("data-accent-color","dull");
+    //     btn.className = "Thio3V0imwc- _0DdgBbU2bPk- _2RWLTCLE-0s- krQEbDRNCFg-";
+    //     btn.textContent = ( document.querySelectorAll(".rQvmBxj2Kvg-:empty").length ) ? "Subscribe to all" : "Unsubscribe to all";
+    //     btn.onclick = async (e)=>{
+        GM_registerMenuCommand("Subscribe/Unsubscribe to all", async (e)=>{
+            console.log(e);
             const unsubscribeditems = document.querySelectorAll(".rQvmBxj2Kvg-:empty");
             if ( unsubscribeditems.length ) {
-                btn.textContent = "Subscribing...";
-                btn.style.pointerEvents = 'none';
+                // btn.textContent = "Subscribing...";
+                // btn.style.pointerEvents = 'none';
                 unsubscribeditems.forEach( async (i,n)=>{
                     await i.parentElement.querySelector("button").click();
                     console.log(`${n.toString().padStart(2,0)}) Subscribed to item id: ${i.id.match(/(\d+)/)[0]}`);
                 });
-                btn.textContent = "Unsubscribe to all";
-                btn.style.pointerEvents = '';
+                // btn.textContent = "Unsubscribe to all";
+                // btn.style.pointerEvents = '';
             } else {
                 const subscribeditems = document.querySelectorAll(".rQvmBxj2Kvg-:not(:empty)");
-                btn.textContent = "Unsubscribing...";
-                btn.style.pointerEvents = 'none';
+                // btn.textContent = "Unsubscribing...";
+                // btn.style.pointerEvents = 'none';
                 subscribeditems.forEach( async (i,n)=>{
                     await i.parentElement.querySelector("button").click();
                     console.log(`${n.toString().padStart(2,0)}) Unsubscribed to item id: ${i.id.match(/(\d+)/)[0]}`);
                 });
-                btn.textContent = "Subscribe to all";
-                btn.style.pointerEvents = '';
+                // btn.textContent = "Subscribe to all";
+                // btn.style.pointerEvents = '';
             }
-        };
-        const btndiv = document.createElement("div");
-        btndiv.appendChild(btn);
-        const lsection = document.querySelector("._2u-CXYgdzlo-");
-        if ( document.querySelectorAll(".BfmIgp5hDLQ- > div > div > button").length ) {
-            if ( lsection ) lsection.querySelector("button.Thio3V0imwc-._0DdgBbU2bPk-._2RWLTCLE-0s-.krQEbDRNCFg-").insertAdjacentElement("afterend",btndiv);
-            else document.querySelector('.VsQBaHlz-3M-').insertAdjacentElement("beforeend",btndiv);
-        }
-    });
+        }, 'E');
+    //  };
+    //     const btndiv = document.createElement("div");
+    //     btndiv.appendChild(btn);
+    //     const lsection = document.querySelector("._2u-CXYgdzlo-");
+    //     if ( document.querySelectorAll(".BfmIgp5hDLQ- > div > div > button").length ) {
+    //         if ( lsection ) lsection.querySelector("button.Thio3V0imwc-._0DdgBbU2bPk-._2RWLTCLE-0s-.krQEbDRNCFg-").insertAdjacentElement("afterend",btndiv);
+    //         else document.querySelector('.VsQBaHlz-3M-').insertAdjacentElement("beforeend",btndiv);
+    //     }
+    // });
 })();
